07734031244b1c0c8df7a7610a880466daec6c39
Лхагвасүрэн Ариунболд <24LP2618@aiRo.local>
2024-08-28 14:32:42 -0700

    Header heseg

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/Header";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center ">
+      <div className="container ">
+        <Header />
+      </div>
+    </div>
+  );
+}
