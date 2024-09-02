22c1601c3917a1b2db42dc2bca2fdeeeb0af2e26
Лхагвасүрэн Ариунболд <24LP2618@aiRo.local>
2024-09-02 13:28:01 -0700

    Test uguv

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,8 +3,18 @@ import { Carousel } from "../carousel/Carousel";
 import { Footer } from "../footer/Footer";
 import { Header } from "../header/Header";
 import { Trending } from "../trending/Trending";
-
+import { useState, useEffect } from "react";
 export default function HomePage() {
+  const [BlogCard, setBlogCard] = useState([]);
+  const fetchData = () => {
+    fetch(" https://dev.to/api/articles")
+      .then((response) => response.json())
+      .then((data) => setBlogCard(data));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="flex flex-col items-center ">
       <div className="container max-w-[1220px] ">
