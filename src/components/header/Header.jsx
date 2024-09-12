b082e426090010229d8e0218416b1355d9abaffd
Лхагвасүрэн Ариунболд <24LP2618@aiRo.local>
2024-09-12 13:50:22 -0700

    Search heseg design saijruulaw

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,6 +9,11 @@ export const Header = () => {
   const filteredArticles = articlesForSearch?.filter((article) =>
     article?.title?.toLowerCase().includes(searchValue)
   );
+  // if (typeof window !== "undefined") {
+  //   document.addEventListener("mousedown", () => {
+  //     searchValue("");
+  //   });
+  // }
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
   };
@@ -20,7 +25,7 @@ export const Header = () => {
   useEffect(() => {
     fetchSearchData();
   }, []);
-  console.log(articlesForSearch);
+
   return (
     <div className="container flex  p-9 justify-between ">
       <div>
@@ -31,25 +36,32 @@ export const Header = () => {
         <Link href="/blog-list">Blog</Link>
         <Link href="/contact-us">Contact</Link>
       </div>
-      <div className="border rounded-md flex flex-col items-center h-5  gap-3 p-4 bg-gray-200">
-        <input
-          onChange={handleInputChange}
-          className="bg-gray-200 w-28 "
-          type="text"
-          placeholder="Search"
-          name=""
-          id=""
-        />
-        <div className="flex flex-col">
-          {searchValue &&
-            filteredArticles.map((article) => {
-              filteredArticles.length = 5;
-              return (
-                <Link href={`/blog-list/${article.id}`}>
-                  <div className="w-[186px] line-clamp-1">{article?.title}</div>
-                </Link>
-              );
-            })}
+      <div className="border rounded-md flex  items-center bg-gray-200 p-2 gap-3">
+        <div className="flex flex-col relative  w-28 ">
+          <input
+            onChange={handleInputChange}
+            className=" w-28 bg-gray-200 outline-none "
+            type="text"
+            placeholder="Search"
+            value={searchValue}
+          />
+          <div
+            className={` ${
+              searchValue ? "h-[200px]" : "h-0"
+            } flex  bg-gray-200  w-[200px] overflow-hidden transition-all duration-200  flex-col absolute top-10 left-0 rounded  `}
+          >
+            {searchValue &&
+              filteredArticles.map((article) => {
+                filteredArticles.length = 5;
+                return (
+                  <Link href={`/blog-list/${article.id}`}>
+                    <div className="line-clamp-1 p-1 box-border ">
+                      {article?.title}
+                    </div>
+                  </Link>
+                );
+              })}
+          </div>
         </div>
 
         <SearchIcon />
